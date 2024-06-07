445a6c65b4233ccc673bb46e4ae249ed86859ab8
davitgithubkedua <hwhwkeowje@gmail.com>
2024-06-07 09:00:03 +0700

    fix error update stock

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -60,7 +60,7 @@
                     {!! $product->short_description !!}
                     </div>
                     
-                    {{-- <a href="javascript:void(0);" onclick="addToCart({{ $product->id }});" class="btn btn-dark"><i class="fas fa-shopping-cart"></i> &nbsp;ADD TO CART</a> --}}
+                    <!-- {{-- <a href="javascript:void(0);" onclick="addToCart({{ $product->id }});" class="btn btn-dark"><i class="fas fa-shopping-cart"></i> &nbsp;ADD TO CART</a> --}} -->
 
                     @if($product->track_qty == 'Yes')
                         @if($product->qty > 0)
@@ -68,11 +68,11 @@
                             <i class="fa fa-shopping-cart"></i> &nbsp;Add To Cart
                         </a>
                         @else 
-                        <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                        <a class="btn btn-dark" href="javascript:void(0);">
                             Stok Habis
                         </a>
                         @endif
-                                   
+                            
                     @else
                     <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
                         <i class="fa fa-shopping-cart"></i> &nbsp;Add To Cart
@@ -137,23 +137,23 @@
                         <a class="whishlist" href="222"><i class="far fa-heart"></i></a>
 
                         <div class="product-action">
-                            {{-- <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                            <!-- {{-- <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
                                 <i class="fa fa-shopping-cart"></i> Add To Cart
-                            </a> --}}
+                            </a> --}} -->
 
-                            @if($product->track_qty == 'Yes')
-                                @if($product->qty > 0)
-                                <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                            @if($relProduct->track_qty == 'Yes')
+                                @if($relProduct->qty > 0)
+                                <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $relProduct->id }});">
                                     <i class="fa fa-shopping-cart"></i> Add To Cart
                                 </a>
                                 @else 
-                                <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                                <a class="btn btn-dark" href="javascript:void(0);">
                                     Stok Habis
                                 </a>
                                 @endif
                                    
                             @else
-                            <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $product->id }});">
+                            <a class="btn btn-dark" href="javascript:void(0);" onclick="addToCart({{ $relProduct->id }});">
                                 <i class="fa fa-shopping-cart"></i> Add To Cart
                             </a>
                             @endif
